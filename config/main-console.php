129ff0bee14c578ce448d6e30590bfd44ad0f6d2
Semenov <semenov@skeeks.com>
2016-09-14 17:29:05 +0300

    dev

--- config/main-console.php
+++ config/main-console.php
@@ -1,2 +1,29 @@
 <?php
-return [];
+return [
+    'components' =>
+    [
+        'cmsImport' => [
+            'class'     => 'skeeks\cms\import\ImportComponent',
+        ],
+
+        'i18n' => [
+            'translations' =>
+            [
+                'skeeks/import' => [
+                    'class'             => 'yii\i18n\PhpMessageSource',
+                    'basePath'          => '@skeeks/cms/import/messages',
+                    'fileMap' => [
+                        'skeeks/import' => 'main.php',
+                    ],
+                ]
+            ]
+        ]
+    ],
+
+    'modules' =>
+    [
+        'cmsImport' => [
+            'class'         => 'skeeks\cms\import\ImportModule',
+        ]
+    ]
+];
