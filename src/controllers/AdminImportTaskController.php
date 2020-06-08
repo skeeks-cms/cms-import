4ff81aa6112a79dee6e6ca292e5b930b42a53802
Semenov <semenov@skeeks.com>
2020-06-09 00:28:20 +0300

    dev

--- src/controllers/AdminImportTaskController.php
+++ src/controllers/AdminImportTaskController.php
@@ -38,6 +38,8 @@ class AdminImportTaskController extends BackendModelStandartController
     {
         return ArrayHelper::merge(parent::actions(), [
             'index' => [
+                'filters' => false,
+                'backendShowings' => false,
                 'grid' => [
                     'visibleColumns' => [
                         'checkbox',
